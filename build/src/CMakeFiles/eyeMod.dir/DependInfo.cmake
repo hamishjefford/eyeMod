10b3da4309ed750d08cc3e48f197a564fc17fc38
hamishjefford <hamish@chester.com>
2014-11-18 14:42:47 +0000

    Initial Commit

---
+++ build/src/CMakeFiles/eyeMod.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hamishjefford/eyeMod/src/findEyeCenter.cpp" "/Users/hamishjefford/eyeMod/build/src/CMakeFiles/eyeMod.dir/findEyeCenter.cpp.o"
+  "/Users/hamishjefford/eyeMod/src/findEyeCorner.cpp" "/Users/hamishjefford/eyeMod/build/src/CMakeFiles/eyeMod.dir/findEyeCorner.cpp.o"
+  "/Users/hamishjefford/eyeMod/src/helpers.cpp" "/Users/hamishjefford/eyeMod/build/src/CMakeFiles/eyeMod.dir/helpers.cpp.o"
+  "/Users/hamishjefford/eyeMod/src/main.cpp" "/Users/hamishjefford/eyeMod/build/src/CMakeFiles/eyeMod.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/src/CMakeFiles/eyeMod.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/eyeMod.dir/main.cpp.o"
+  "CMakeFiles/eyeMod.dir/findEyeCenter.cpp.o"
+  "CMakeFiles/eyeMod.dir/findEyeCorner.cpp.o"
+  "CMakeFiles/eyeMod.dir/helpers.cpp.o"
+  "../bin/eyeMod.pdb"
+  "../bin/eyeMod"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/eyeMod.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
